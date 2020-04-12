9c3e31d9db3f92dec7e2fabb9082ecc617ef8261
webVueBlog <2979986590@qq.com>
2020-04-12 16:17:22 +0800

    08

--- VueRouter/class06/index.js
+++ VueRouter/class06/index.js
@@ -1,4 +1,54 @@
+const Home = {
+	methods:{
+		toA() {
+			this.$router.push('/a')
+		},
+		toB() {
+			this.$router.push('/b')
+		}
+	},
+	template: `,
+	<div>
+		<h1>Home</h1>
+		<button @click="toA">toA</button>
+		<button @click="toB">toB</button>
+	</div>
+	`
+}
 
+const CompB = {
+	template: `,
+	<div>
+		<h1>CompB</h1>
+	</div>
+	`
+}
+
+
+const router = new VueRouter({
+	routes: [{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/b',
+			component: CompB,
+			alias: '/123'
+		},
+		{
+			path: '/a',
+			redirect: '/b'
+		},
+		{
+			path: '/c',
+			redirect: ()=> '/b'
+		},
+		{
+			path: '/d',
+			redirect: ()=> {return '/b'}
+		}
+	]
+})
 
 new Vue({
 	el: '#app',
