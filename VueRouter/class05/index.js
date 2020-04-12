694d9eac545a222fb1f8b32d14933ecbb265f8fc
webVueBlog <2979986590@qq.com>
2020-04-12 14:17:51 +0800

    06

--- VueRouter/class05/index.js
+++ VueRouter/class05/index.js
@@ -1,3 +1,53 @@
+const Home = {
+	methods: {
+		gotoUser() {
+			this.$router.push({
+				name: 'user',
+				params: {userId:"123456"}
+			})
+		},
+		gotoUserReplace() {
+			this.$router.replace({
+				name: 'user',
+				params: {userId: '888'}
+			})
+		}
+	},
+	template: `,
+	<div>
+		<h1>Home</h1>
+		<button @click="gotoUser">gotoUser</button>
+		<router-link :to="{ name: 'user', params: { userId: 123 }}">User</router-link>
+		
+		<button @click="gotoUserReplace">gotoUserReplace</button>
+		<router-link :to="{ name: 'user', params: { userId: 123 }}" replace>User</router-link>
+	</div>
+	`
+	
+}
+
+const User = {
+	template: `,
+		<div>
+			User {{$route.params.userId}}
+		</div>
+	`
+}
+
+const router = new VueRouter({
+	routes: [
+		{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/user/:userId',
+			name: 'user',
+			component: User
+		}
+	]
+})
+
 
 new Vue({
 	el: '#app',
